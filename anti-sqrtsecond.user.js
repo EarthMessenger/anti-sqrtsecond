247496122ca590054c4987729526efa3aa064fd9
earthmessenger <earthmessenger@qq.com>
2023-05-26 15:18:29 +0800

    style: 重写代码
    改用 css 伪元素标示诈骗链接
    
    todo: 还没做链接匹配检测，但我觉得这个没必要

--- anti-sqrtsecond.user.js
+++ anti-sqrtsecond.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Luogu Feed: Anti-SqrtSecond
 // @namespace    https://imken.moe/
-// @version      0.1.5.0
+// @version      0.2.0
 // @description  某些人总是喜欢发一些隐藏犇犇 e.g. `[](你被骗了)`
 // @author       Imken Luo
 // @match        https://www.luogu.com.cn/
@@ -10,85 +10,110 @@
 // @grant        none
 // ==/UserScript==
 
-const keywordMap = {
-    'BV1GJ411x7h7': 'RickRoll',
-    'milime.top': 'RickRoll',
-    'rrHrxMt': 'RickRoll',
-    '192d9a98d782d9c74c96f09db9378d93.mp4': 'RickRoll',
-    'BV12x411y7SN': '洛天依 - 凉雨',
-    'BV1sx411S7rN': '天依教你甜甜圈的正确用法',
+const contentMap = {
+    'rickroll': 'RickRoll',
+    'bilibli1': '洛天依 - 凉雨',
+    'bilibili2': '天依教你甜甜圈的正确用法',
 };
 
-(function() {
+const keywordMap = {
+    'BV1GJ411x7h7': 'rickroll',
+    'milime.top': 'rickroll',
+    'rrHrxMt': 'rickroll',
+    '192d9a98d782d9c74c96f09db9378d93.mp4': 'rickroll',
+    'BV12x411y7SN': 'bilibli1',
+    'BV1sx411S7rN': 'bilibili2',
+};
+
+// 根据 contentMap 生成 css
+const genCss = (contentMap) => {
+    let res = "";
+    for (let i in contentMap) {
+        res += `.as-${i}::after {
+            content: "[Warn: ${contentMap[i]}]"; 
+            margin-left: 0.2em;
+            font-family: monospace;
+            background-color: yellow;
+            color: red;
+        };\n`;
+    }
+    return res;
+};
+
+// 注入 css
+const addCSS = (css) => {
+    let style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    document.head.appendChild(style);
+};
+
+const markBenbenA = (ele) => {
+    const href = ele.getAttribute('href');
+    if (ele.innerHTML.trim() === '') {
+        ele.innerHTML = href;
+    }
+    for (let key in keywordMap) {
+        if (href.includes(key)) {
+            ele.classList.add(`as-${keywordMap[key]}`);
+            ele.classList.add('as-warn');
+            break;
+        }
+    }
+    console.log(ele);
+};
+
+const markBenbenImg = (ele) => {
+    let altText = ele.getAttribute('alt');
+    let src = ele.getAttribute('src');
+    src = decodeURIComponent(src);
+    let newAltText = `${altText} | ${src}`;
+    ele.setAttribute('alt', newAltText);
+    ele.setAttribute('title', newAltText);
+    console.log(ele);
+};
+
+// 当观察到变动时执行的回调函数
+const observerCallback = (mutationsList, observer) => {
+    for (let mutation of mutationsList) {
+        for (let addedNode of mutation.addedNodes) {
+            if (!addedNode.querySelectorAll) continue;
+
+            aElements = addedNode.querySelectorAll('.feed-comment a');
+            for (let ele of aElements) {
+                if (ele.hasAttribute("as-marked")) continue;
+                ele.setAttribute("as-marked", "1");
+                markBenbenA(ele);
+            }
+
+            imgElements = addedNode.querySelectorAll('#feed .feed-comment img');
+            for (let ele of imgElements) {
+                if (ele.hasAttribute("as-marked")) continue;
+                ele.setAttribute("as-marked", "1");
+                markBenbenImg(ele);
+            }
+
+        }
+    }
+}
+
+(() => {
+    addCSS(genCss(contentMap));
+
     // 选择需要观察变动的节点
     const targetNode = document.getElementById('feed');
-    if (targetNode) {
-        // 观察器的配置（需要观察什么变动）
-        const config = { attributes: false, childList: true, subtree: false };
 
-        // 当观察到变动时执行的回调函数
-        const callback = function(mutationsList, observer) {
-            for (let mutation of mutationsList) {
-                if (mutation.type === 'childList') {
-                    // 选择所有犇犇下的文字
-                    const urlElements = document.querySelectorAll('.feed-comment p');
+    // 没有犇犇不执行
+    if (targetNode === null) return ;
 
-                    // 遍历每一个元素
-                    for (let i in urlElements) {
-                        if (urlElements[i].innerHTML && !urlElements[i].getAttribute('vist')) {
-                            urlElements[i].setAttribute('vist', '1');
-                            // 正则表达式替换
-                            urlElements[i].innerHTML = urlElements[i].innerHTML.replace(/<a\s+[^>]*href="([^"]*)"[^>]*>(\s*|&nbsp;)<\/a>/g, function(match, href) {
-                                // 替换为内容为 href 的 <span> 标签
-                                return '<span style="background-color: black; color: white;">' + decodeURIComponent(href) + '</span>';
-                            });
-                        }
-                    }
+    // 观察器的配置（需要观察什么变动）
+    const config = { attributes: false, childList: true, subtree: false };
 
-                    // 获取所有图片元素
-                    const imgElements = document.querySelectorAll('.feed-comment img');
+    // 创建一个观察器实例并传入回调函数
+    const observer = new MutationObserver(observerCallback);
 
-                    // 遍历每个图片元素
-                    for (let i in imgElements) {
-                        if (imgElements[i].getAttribute && !imgElements[i].getAttribute('vist')) {
-                            var altText = imgElements[i].getAttribute('alt');
-                            var src = imgElements[i].getAttribute('src');
-                            src = decodeURIComponent(src);
-                            var newAltText = altText + ' | ' + src;
-                            imgElements[i].setAttribute('alt', newAltText);
-                            imgElements[i].setAttribute('style', 'background: gray; color: white;');
-                            imgElements[i].setAttribute('vist', '1');
-                        }
-                    }
-
-                    const linkElements = document.querySelectorAll('.feed-comment p a');
-                    for (let i in linkElements) {
-                        if (!linkElements[i].href) break;
-                        if (linkElements[i].getAttribute('vist')) continue;
-                        let link = linkElements[i].href;
-                        let text = linkElements[i].innerText;
-                        let flag = 1;
-                        for (let keyword in keywordMap) {
-                            if (link.includes(keyword)) {
-                                linkElements[i].innerHTML += '<span style="background-color: yellow; font-family: monospace; color: red;"> [Warn: ' + keywordMap[keyword] + ']</span>';
-                                linkElements[i].style['background-color'] = 'yellow';
-                                flag = 0;
-                                break;
-                            }
-                        }
-                        if (flag && (text.startsWith('http://') || text.startsWith('https://')) && (link != text)) {
-                            linkElements[i].innerHTML += '<span style="background-color: yellow; font-family: monospace; color: red;"> [Warn: 链接不匹配]</span>';
-                            linkElements[i].style['background-color'] = 'yellow';
-                        }
-                        linkElements[i].setAttribute('vist', '1');
-                    }
-                }
-            }
-        };
-        // 创建一个观察器实例并传入回调函数
-        const observer = new MutationObserver(callback);
-
-        // 以上述配置开始观察目标节点
-        observer.observe(targetNode, config);
-    }
+    // 以上述配置开始观察目标节点
+    observer.observe(targetNode, config);
 })();
+
+// vim:et:sts=4:sw=4
